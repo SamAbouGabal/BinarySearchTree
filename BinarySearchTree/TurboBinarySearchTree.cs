583d2f510402203140567e78ebedf59ec334eb2f
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-31 14:02:46 +0200

    Update TurboBinarySearchTree.cs

--- BinarySearchTree/TurboBinarySearchTree.cs
+++ BinarySearchTree/TurboBinarySearchTree.cs
@@ -1,6 +1,27 @@
 namespace BinarySearchTree;
 
-public class TurboBinarySearchTree
+public class TurboBinarySearchTree<T> where T : IComparable<T>
 {
-    
+    public void Insert(T)
+    {
+        
+    }
+
+    public bool Search(T)
+    {
+        
+    }
+
+    public bool Delete(T)
+    {
+        
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        
+    }
+
+
+
 }
