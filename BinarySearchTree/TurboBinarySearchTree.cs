faaaabc6e5c3ee0e7f5a092435aa23c21151d42b
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-06-01 18:21:28 +0200

    Update TurboBinarySearchTree.cs

--- BinarySearchTree/TurboBinarySearchTree.cs
+++ BinarySearchTree/TurboBinarySearchTree.cs
@@ -2,17 +2,59 @@ namespace BinarySearchTree;
 
 public class TurboBinarySearchTree<T> where T : IComparable<T>
 {
-    public void Insert(T)
+    private class Node
+    {
+        public T Value;
+        public Node Right;
+        public Node Left;
+
+        public Node(T value)
+        {
+            Value = value;
+            Right = null;
+            Left = null;
+        }
+    }
+
+    private Node root;
+
+    public TurboBinarySearchTree()
+    {
+        root = null;
+    }
+    
+    public void Insert(T value)
+    {
+        root = Insert(root, value);
+    }
+
+    private Node Insert(Node node, T value)
+    {
+        if (node == null)
+        {
+            return new Node(value);
+        }
+
+        int compare = value.CompareTo(node.Value);
+
+        if (compare < 0)
+        {
+            node.Left = Insert(node.Left, value);
+        }
+        else if (compare > 0)
+        {
+            node.Right = Insert(node.Right, value);
+        }
+
+        return node;
+    }
+
+    public bool Search(T value)
     {
         
     }
 
-    public bool Search(T)
-    {
-        
-    }
-
-    public bool Delete(T)
+    public bool Delete(T value)
     {
         
     }
