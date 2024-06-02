7073fb0b4214eb821bcf16c3bbfc40c35b0cce83
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-06-02 17:47:19 +0200

    Update Program.cs

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,12 +1,11 @@
-﻿using BinarySearchTree;
+﻿using System;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main(string[] args)
     {
         TurboBinarySearchTree<int> tree = new TurboBinarySearchTree<int>();
 
-        // fill the tree with a lot of even numbers
         for (int i = 0; i < 1000000; i += 2)
         {
             tree.Insert(i);
@@ -19,12 +18,16 @@ class Program
         Console.WriteLine("Deleted 50001: " + tree.Delete(50001)); // should return false
         Console.WriteLine("Found 50001: " + tree.Search(50001)); // should return false
 
-        // this number is now in the tree twice
         tree.Insert(50002);
         Console.WriteLine("Found 50002: " + tree.Search(50002)); // should return true
         Console.WriteLine("Deleted 50002: " + tree.Delete(50002)); // should return true
         Console.WriteLine("Found 50002: " + tree.Search(50002)); // should return true
         Console.WriteLine("Deleted 50002: " + tree.Delete(50002)); // should return true
         Console.WriteLine("Found 50002: " + tree.Search(50002)); // should return false
+
+        foreach (var value in tree)
+        {
+            Console.WriteLine(value);
+        }
     }
 }
